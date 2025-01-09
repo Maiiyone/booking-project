8dd89be13ecc76c65ef5913120ce7e76206c187a
Maiiyone <21522322@gm.uit.edu.vn>
2025-01-09 11:25:29 +0700

    change Color global

--- src/utils/shared/Colors.js
+++ src/utils/shared/Colors.js
@@ -1,14 +1,13 @@
 // colors.js
 const Colors = {
-    primary: '#003580',
-    secondary: '#2ecc71',
-    accent: '#9b59b6',
-    background: '#f5f5f5',
-    text: '#333333',
-    error: '#e74c3c',
-    warning: '#f1c40f',
-    success: '#2ecc71',
-  };
-  
-  export default Colors;
-  
+  primary: '#003580', // Màu xanh dương đậm, màu chính
+  secondary: '#FFD700', // Màu vàng nhấn
+  accent: '#0071C2', // Xanh dương sáng để tạo điểm nhấn
+  background: '#FFFFFF', // Màu nền trắng
+  text: '#4F4F4F', // Màu xám đậm cho văn bản
+  error: '#FF3B30', // Màu đỏ để hiển thị lỗi
+  warning: '#FF9500', // Màu cam để cảnh báo
+  success: '#34C759', // Màu xanh lá cho trạng thái thành công
+};
+
+export default Colors;
